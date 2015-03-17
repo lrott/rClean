53030953799a6e366b76d7b591cce33d44ddde40
Logan <loganrott@gmail.com>
2015-03-17 13:18:13 -0400

    Update rClean.r

--- rClean.r
+++ rClean.r
@@ -1,13 +1,17 @@
 
-# generic text field
-function(x) {
+# generic string
+function(x, y) {
   
-  x <- trim(x)
-  x <- tolower(x)
-
-}
-
-# phone
-x <- gsub("[[:punct:]]", "", x)
-
-# email
+  if(y = "phone") {
+    x <- gsub("[[:punct:]]", "", x) # punct
+    x <- gsub("[^0-9]", "", x) # non-numerics
+    x <- gsub(" ", "", x) # white space
+  }
+  elseif(y = "string" {
+    x <- trim(x)
+    x <- tolower(x)
+    x <- gsub("[[:punct:]]", "", x)
+  }
+  else {
+    print("Enter phone or string")
+  }
